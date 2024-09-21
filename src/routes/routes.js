5b17e09ce27e2f3ec5c2c85eefa9378c811aa2ed
Francisco López García Mayorga <javier6282@hotmail.com>
2024-09-20 22:13:53 -0500

    [UPDATE] create route to get specific pokemon and return their name and types.

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import axios from "axios";
+import e from "express";
 
 const router = express.Router();
 
@@ -7,17 +8,35 @@ const path = "/";
 const api = "https://pokeapi.co/api/v2/pokemon";
 
 // Obtener los 100 primeros pokemons
-router.get(path, (req, res) => {
-  axios
-    .get(`${api}/?limit=100&offset=100`)
-    .then((response) => {
-      const pokemons = response.data;
-      res.send(pokemons);
-    })
-    .catch((error) => {
-      console.error(error);
-      res.status(500).send({
-        message: "Error al obtener la lista de pokemons",
-      });
+router.get(path, async (req, res) => {
+  try {
+    const response = await axios.get(`${api}?limit=100`);
+    res.status(response.status).json(response.data);
+  } catch (error) {
+    const { status, statusText } = error.response || {
+      status: 500,
+      statusText: "Internal Server Error",
+    };
+    res.status(status).json({
+      message: `Error al obtener la lista de pokemons: ${statusText}`,
     });
+  }
 });
+
+// Obtener un pokemon por ID
+router.get(`${path}:id`, async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { data, status } = await axios.get(`${api}/${id}`);
+    const { name, types } = data;
+    res.status(status).json({ name, types });
+  } catch (error) {
+    const { status = 500, statusText = "Error interno del servidor" } =
+      error.response || {};
+    res
+      .status(status)
+      .json({ message: `Error al obtener el pokemon: ${statusText}` });
+  }
+});
+
+export default router;
